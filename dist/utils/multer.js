ebd275048b273bbca6546d819a36bc3bbb5145a6
mhasan1122 <mirzahasanlimon619@gmail.com>
2025-04-22 14:18:15 +0600

    feat: Implement core models and routes for the application
    - Added middleware for user authentication (isLoggedIn).
    - Created models for ContactForm, Refund, Review, Category, Content, Delivery, Order, Payment, Product, SubCategory, and User.
    - Developed routes for authentication, categories, contact forms, content management, deliveries, orders, payments, products, refunds, reviews, and subcategories.
    - Integrated multer for file uploads in relevant routes.
    - Implemented email service for sending OTP and contact form submissions.
    - Configured Cloudinary for image and video uploads.
    - Added utility functions for generating OTPs and JWT tokens.

---
+++ dist/utils/multer.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const multer_1 = __importDefault(require("multer"));
+const path_1 = __importDefault(require("path"));
+const storage = multer_1.default.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(null, file.fieldname + "-" + uniqueSuffix + path_1.default.extname(file.originalname));
+    },
+});
+const fileFilter = (req, file, cb) => {
+    const filetypes = /jpeg|jpg|png|gif|mp4|mov|avi|webm/;
+    const extname = filetypes.test(path_1.default.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+    if (extname && mimetype) {
+        return cb(null, true);
+    }
+    else {
+        cb(new Error("Only image (jpeg, jpg, png, gif) and video (mp4, mov, avi, webm) files are allowed!"));
+    }
+};
+const upload = (0, multer_1.default)({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 100 * 1024 * 1024,
+    },
+});
+exports.default = upload;
